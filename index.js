a347b81972de133d72389fd7726b841e09c08291
Fatih <justicestranger@gmail.com>
2022-09-04 18:17:39 +0300

    The project 1 of Backend Dev & Apis !failed successfully.

--- index.js
+++ index.js
@@ -2,13 +2,14 @@
 // where your node app starts
 
 // init project
+require('dotenv').config();
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,10 +19,30 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date?", (req, res) => {
+  const theval = req.params.date;
+  const isStr = isNaN(Number(req.params.date))
+  console.log(`is string? ${isStr}, typeof: ${typeof parseInt(req.params.date)}, value is: ${req.params.date}`);
+  const dateNumVal = Number(req.params.date); 
+  if (theval && !isStr) {
+    var unixSec = new Date(dateNumVal).getTime();
+    var dateGMT = new Date(dateNumVal).toGMTString();
+  } else if (theval && isStr) {
+    var unixSec = new Date(req.params.date).getTime();
+    var dateGMT = new Date(req.params.date).toGMTString();
+  } else if (!theval) {
+    var unixSec = new Date().getTime();
+    var dateGMT = new Date().toGMTString();
+  }
+  if (dateGMT == "Invalid Date") res.json({ "error": dateGMT })
+  else {
+    res.json({ "unix": unixSec, "utc": dateGMT })
+  }
+})
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
 
